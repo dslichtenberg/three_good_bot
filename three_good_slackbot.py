ffd435e1c7e14e6645344c1d2b666eed5da20057
dan lichtenberg <dan@keikicapital.com>
2020-03-06 14:48:43 -0800

    march changes

--- three_good_slackbot.py
+++ three_good_slackbot.py
@@ -1,13 +1,31 @@
 import time, sys, re
 import slack
-
+import math
 import config_file as cfg
 
 from absl import flags
 
 
 
-def sendDMtoAllUsers(customer_name, message):
+def sendDMtoUser(customer_codes,user_name, customer_name, message):
+  slackclient= slack.WebClient(token =  customer_codes[customer_name])
+  members = slackclient.users_list()['members']
+  
+  for member in members:
+    if member['name'] == user_name:
+      
+      conversation_id = slackclient.conversations_open(users=  member['id'])
+      slackclient.chat_postMessage(  
+        channel = conversation_id['channel']['id'] ,
+        text = f':potable_water::potable_water:Hi, {user_name}!:potable_water::potable_water:\n {message} '
+      )
+      #slackclient.files_upload (channel = conversation_id['channel']['id'], 
+      #  file  =   './content/geckoboard_snap.png',
+      #  title = "today's 3 good stats")
+      #slackclient.files_sharedPublicURL(id =  conversation_id['channel']['id'] )
+  return f'Sent message "{message}" to users: {[member["name"]  for member in members if member["name"] == user_name]} at client {customer_name}'
+
+def sendDMtoAllUsers(customer_codes,customer_name, message):
   slackclient= slack.WebClient(token =  customer_codes[customer_name])
   members = slackclient.users_list()['members']
   for member in members:
@@ -16,7 +34,7 @@ def sendDMtoAllUsers(customer_name, message):
       conversation_id = slackclient.conversations_open(users=  member['id'])
       slackclient.chat_postMessage(  
         channel = conversation_id['channel']['id'], 
-        text = f':potable_water::potable_water:Hi, {name}! {message}:potable_water::potable_water:'
+        text = f':potable_water::potable_water:Hi, {name}! {message} :potable_water::potable_water:'
       )
   return f'Sent message "{message}" to users: {[member["name"] for member in members]} at client {customer_name}'
 
@@ -30,8 +48,12 @@ def main():
 
   for i in range(msg_queue.message.count()):
     if(msg_queue.sent[i] != True): 
-      print(sendDMtoAllUsers(msg_queue.client_to[i].strip(), msg_queue.message[i].strip()))
-      #cfg.update_messaging_status()
+      if(math.isnan(msg_queue.user[i])):
+        print(sendDMtoAllUsers(customer_codes, msg_queue.client_to[i].strip(), msg_queue.message[i].strip()))
+      else:
+        print(sendDMtoUser(customer_codes,msg_queue.user[i].strip(), msg_queue.client_to[i].strip(), msg_queue.message[i].strip()))
+      #
+      cfg.update_messaging_status()
 
 if __name__ == '__main__':
   main()
